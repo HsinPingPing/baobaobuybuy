9432c14565a6610d01224408c48a5a320ed8791f
Hsin-Ping Chen <ping1409@drinkfather-lm.tpcity.corp.yahoo.com>
2016-08-11 10:30:09 +0800

    quick test template receipt

--- app.js
+++ app.js
@@ -265,14 +265,14 @@ function receivedPostback(event) {
 
   console.log("Received postback for user %d and page %d with payload '%s' " + 
     "at %d", senderID, recipientID, payload, timeOfPostback);
+  console.log("Get Message >>>", payload.substring(1));
 
   // When a postback is called, we'll send a message back to the sender to 
   // let them know it was successful
   // var msg = msgs[Math.floor(Math.random() * msgs.length)];
 
   // sendTextMessage(senderID, '已加入 ' + payload + '，' + msg);
-  //baobao(senderID, '', true);
-
+  baobao(senderID, payload.substring(1));
 
 }
 
@@ -314,6 +314,12 @@ function ToKG(recipientId, response, recommendations){
             type: "template",
             payload: {
                 template_type: "receipt",
+                "recipient_name":"Stephane Crozatier",
+                "order_number":"12345678902",
+                "currency":"USD",
+                "payment_method":"Visa 2345",        
+                "order_url":"http://petersapparel.parseapp.com/order?order_id=123456",
+                "timestamp":"1428444852", 
                 elements: [{
                     "title":"Classic White T-Shirt",
                     "subtitle":"100% Soft and Luxurious Cotton",
@@ -328,7 +334,29 @@ function ToKG(recipientId, response, recommendations){
                     "price":25,
                     "currency":"USD",
                     "image_url":"http://petersapparel.parseapp.com/img/grayshirt.png"
-                }]}
+                }]
+                "address":{
+                    "street_1":"1 Hacker Way",
+                    "street_2":"",
+                    "city":"Menlo Park",
+                    "postal_code":"94025",
+                    "state":"CA",
+                    "country":"US"
+                    },
+                "summary":{
+                    "subtotal":75.00,
+                    "shipping_cost":4.95,
+                    "total_tax":6.19,
+                    "total_cost":56.14
+                    },
+                "adjustments":[{
+                    "name":"New Customer Discount",
+                    "amount":20
+                    },{
+                        "name":"$10 Off Coupon",
+                        "amount":10
+                    }]
+            }
         }}};
     //response.message.attachment.payload.elements = recommendations;
     callSendAPI(response);
