6f89311e6d77326fc383038f8bf027c1f2c5d06a
Hsin-Ping Chen <ping1409@drinkfather-lm.tpcity.corp.yahoo.com>
2016-08-11 15:20:53 +0800

    length problem

--- app.js
+++ app.js
@@ -284,7 +284,7 @@ function baobao(recipientId, messageText) {
     request.get(flaskBackend, function(error, response, body) {
         console.log("baobao body >>>>", body);
         var recommendations = JSON.parse(body);
-        if (typeof body == 'undefined' || body == null ){
+        if (typeof body == 'undefined' || body === null ){
             baobao_useless(recipientId, response);
         }else if(recommendations.length == 0){
             baobao_useless(recipientId, response);
